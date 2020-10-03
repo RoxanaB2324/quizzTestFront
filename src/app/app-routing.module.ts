682441f0ac9f8f2b383f85e2b70f35130e5c6e59
RoxanaB2324 <roxana.briciag@gmail.com>
2020-10-03 13:52:35 +0300

    Register quizz component in router outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QuizzListComponent } from './quizz-list/quizz-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: QuizzListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
