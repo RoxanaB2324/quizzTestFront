11a197171fa412f6068f8f96b96c02a089b2a121
RoxanaB2324 <roxana.briciag@gmail.com>
2020-10-04 11:09:42 +0300

    modif rout & question-list.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { QuizzListComponent } from './quizz-list/quizz-list.component';
-
+import { QuestionListComponent } from './question-list/question-list.component';
 
 const routes: Routes = [
-  {path:'', component: QuizzListComponent}
+  {path:'quizz', component: QuizzListComponent},
+  {path:'quest', component: QuestionListComponent}
 ];
 
 @NgModule({
--- src/app/question-list/question-list.component.ts
+++ src/app/question-list/question-list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import {QuestionListService} from './question-list.service';
 @Component({
   selector: 'app-question-list',
   templateUrl: './question-list.component.html',
   styleUrls: ['./question-list.component.css']
 })
+
+
 export class QuestionListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service : QuestionListService) {  }
+
+  questionsToDisplay = [];
 
   ngOnInit(): void {
+    this.service.getData().subscribe((items: Array<any>)=>{
+      console.log ("Question are:  ", items);
+      this.questionsToDisplay = items;
+    })
   }
 
 }
